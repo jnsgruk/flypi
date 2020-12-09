f28d4ca60ee05c5731ffb5ae905922ab29f4250a
Jon Seager <jonseager@gmail.com>
2020-12-09 22:39:33 +0000

    Updated entrypoint for planefinder

--- planefinder/entrypoint.sh
+++ planefinder/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -euo pipefail 
 
-cat <<-EOF > /home/planefinder/config.json
+if [[ -n "${PLANEFINDER_SHARECODE:-}" ]]; then
+  cat <<-EOF > /home/planefinder/config.json
 {
   "tcp_address": "dump1090",
   "tcp_port": "30005",
@@ -9,10 +11,11 @@ cat <<-EOF > /home/planefinder/config.json
   "connection_type": "1",
   "aircraft_timeout": "30",
   "data_format": "1",
-  "latitude": "$LAT",
-  "longitude": "$LON",
-  "sharecode": "$PLANEFINDER_SHARECODE"
+  "latitude": "${LAT}",
+  "longitude": "${LON}",
+  "sharecode": "${PLANEFINDER_SHARECODE}"
 }
 EOF
+fi
 
 /usr/local/bin/pfclient --config_path=/home/planefinder/config.json
